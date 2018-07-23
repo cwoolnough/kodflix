b6c87f6106e1bac20debda9e4ef1fbd0bb47ef9e
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-07-23 15:28:06 +0100

    Added TV Cover

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Avengers from './Components/Movies/Avengers.jpg';
+import JurassicWorld from './Components/Movies/JurassicWorld.jpg';
+import MammaMia from './Components/Movies/MammaMia.jpg';
+import Rampage from './Components/Movies/Rampage.jpg';
+import Siberia from './Components/Movies/Siberia.jpg';
+import Sicario from './Components/Movies/Sicario.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="movies">
+        <img src={Avengers} alt="Avengers" className="movie"/>
+        <img src={JurassicWorld} alt="Jurassic World" className="movie"/>
+        <img src={MammaMia} alt="Mamma Mia" className="movie"/>
+        <img src={Rampage} alt="Rampage" className="movie"/>
+        <img src={Siberia} alt="Siberia" className="movie"/>
+        <img src={Sicario} alt="Sicario" className="movie"/>
       </div>
     );
   }
