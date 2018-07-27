201ebabdacb1b52a12cd706e718b965a02f534c8
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-07-27 18:25:31 +0100

    Introduce routing / navigation #8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Gallery from './Frontend/Gallery/Gallery';
+import Details from './Frontend/Details/Details';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Gallery />
+        <Router>
+          <div>
+            <Route exact path="/" component={Gallery} />
+            <Route exact path="/Details" component={Details} />
+          </div>
+        </Router>
       </div>
     );
   }
--- src/Frontend/Cover/Cover.js
+++ src/Frontend/Cover/Cover.js
@@ -1,16 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 function Cover(props) {
    return (
-      <div className="cover">
-         <img src={props.src} alt={props.title} className="movie" />
-         <div className="cover-title">
-            <div>
-               <h1>{props.title}</h1>
+      
+         <Link to="Details" className="cover">
+            <img src={props.src} alt={props.title} className="movie" />
+            <div className="cover-title">
+               <div>
+                  <h1>{props.title}</h1>
+               </div>
             </div>
-         </div>
-      </div>
+         </Link>
    )
 }
 
