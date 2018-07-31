3f29df8b0430d8e9c49809beafa700230f67430a
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-07-31 11:26:49 +0100

    Move the covers data model to a proper front-end data source #12

--- src/Frontend/Gallery/Gallery.js
+++ src/Frontend/Gallery/Gallery.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import Avengers from './../Movies/Avengers.jpg';
-import JurassicWorld from './../Movies/JurassicWorld.jpg';
-import MammaMia from './../Movies/MammaMia.jpg';
-import Rampage from './../Movies/Rampage.jpg';
-import Siberia from './../Movies/Siberia.jpg';
-import Sicario from './../Movies/Sicario.jpg';
 import Cover from './../Cover/Cover.js';
 import './../Cover/Cover.css';
+import Movies from './../GalleryGet/GalleryGet';
 
 class Gallery extends Component {
    render() {
       return (
          <div className="container-row">
-            <Cover src={Avengers} title="Avengers" id="_Avengers" />
-            <Cover src={JurassicWorld} title="Jurassic World" id="_Jurassic_World" />
-            <Cover src={MammaMia} title="Mamma Mia" id="_Mamma_Mia" />
-            <Cover src={Rampage} title="Rampage" id="_Rampage"/>
-            <Cover src={Sicario} title="Sicario" id="_Sicario"/>
-            <Cover src={Siberia} title="Siberia" id="_Siberia"/>
+            {Movies.map(movie => <Cover src={movie.src} title={movie.title} alt={movie.title} id={movie.id} key={movie.id} />
+            )}  
          </div>
       );
    }
---
+++ src/Frontend/GalleryGet/GalleryGet.js
@@ -0,0 +1,51 @@
+import Avengers from './../Movies/Avengers.jpg';
+import JurassicWorld from './../Movies/JurassicWorld.jpg';
+import MammaMia from './../Movies/MammaMia.jpg';
+import Rampage from './../Movies/Rampage.jpg';
+import Siberia from './../Movies/Siberia.jpg';
+import Sicario from './../Movies/Sicario.jpg';
+
+const Movies = [
+{
+   src: Avengers,
+   title: "Avengers",
+   alt: "Avengers",
+   id: "_Avengers"
+},
+
+{
+   src: JurassicWorld,
+   title: "Jurassic World",
+   alt: "Jurassic World",
+   id: "_Jurassic_World"
+},
+
+{
+   src: MammaMia,
+   title: "Mamma Mia",
+   alt: "Mamma Mia", 
+   id: "_Mamma_Mia" 
+},
+
+{
+   src: Rampage,
+   title: "Rampage",
+   alt: "Rampage",
+   id: "_Rampage"
+},
+
+{
+   src: Sicario,
+   title: "Sicario",
+   alt: "Sicario",
+   id: "_Sicario"
+},
+
+{
+   src: Siberia,
+   title: "Siberia",
+   alt: "Siberia",
+   id: "_Siberia"
+}];
+
+export default Movies;
