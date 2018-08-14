38fe07ed109abe08f6b1fac22d8466c3413200b8
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-08-14 12:27:55 +0100

    Return a list of shows from the backend #19

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const Movies = require('./movies')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/movies', (req, res) => res.send(Movies))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- src/Frontend/GalleryGet/GalleryGet.js
+++ src/Frontend/GalleryGet/GalleryGet.js
@@ -1,57 +1,57 @@
-import Avengers from './../Movies/Avengers.jpg';
-import JurassicWorld from './../Movies/JurassicWorld.jpg';
-import MammaMia from './../Movies/MammaMia.jpg';
-import Rampage from './../Movies/Rampage.jpg';
-import Siberia from './../Movies/Siberia.jpg';
-import Sicario from './../Movies/Sicario.jpg';
+// import Avengers from './../Movies/Avengers.jpg';
+// import JurassicWorld from './../Movies/JurassicWorld.jpg';
+// import MammaMia from './../Movies/MammaMia.jpg';
+// import Rampage from './../Movies/Rampage.jpg';
+// import Siberia from './../Movies/Siberia.jpg';
+// import Sicario from './../Movies/Sicario.jpg';
 
-const Movies = [
-{
-   src: Avengers,
-   title: "Avengers",
-   alt: "Avengers",
-   id: "_Avengers",
-   synopsis: "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe."
-},
+// const Movies = [
+// {
+//    src: Avengers,
+//    title: "Avengers",
+//    alt: "Avengers",
+//    id: "_Avengers",
+//    synopsis: "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe."
+// },
 
-{
-   src: JurassicWorld,
-   title: "Jurassic World",
-   alt: "Jurassic World",
-   id: "_Jurassic_World",
-   synopsis: "When the island's dormant volcano begins roaring to life, Owen and Claire mount a campaign to rescue the remaining dinosaurs from this extinction-level event."
-},
+// {
+//    src: JurassicWorld,
+//    title: "Jurassic World",
+//    alt: "Jurassic World",
+//    id: "_Jurassic_World",
+//    synopsis: "When the island's dormant volcano begins roaring to life, Owen and Claire mount a campaign to rescue the remaining dinosaurs from this extinction-level event."
+// },
 
-{
-   src: MammaMia,
-   title: "Mamma Mia",
-   alt: "Mamma Mia", 
-   id: "_Mamma_Mia",
-   synopsis: "Five years after the events of Mamma Mia! (2008), Sophie learns about her mother's past while pregnant herself." 
-},
+// {
+//    src: MammaMia,
+//    title: "Mamma Mia",
+//    alt: "Mamma Mia", 
+//    id: "_Mamma_Mia",
+//    synopsis: "Five years after the events of Mamma Mia! (2008), Sophie learns about her mother's past while pregnant herself." 
+// },
 
-{
-   src: Rampage,
-   title: "Rampage",
-   alt: "Rampage",
-   id: "_Rampage",
-   synopsis: "When three different animals become infected with a dangerous pathogen, a primatologist and a geneticist team up to stop them from destroying Chicago."
-},
+// {
+//    src: Rampage,
+//    title: "Rampage",
+//    alt: "Rampage",
+//    id: "_Rampage",
+//    synopsis: "When three different animals become infected with a dangerous pathogen, a primatologist and a geneticist team up to stop them from destroying Chicago."
+// },
 
-{
-   src: Sicario,
-   title: "Sicario",
-   alt: "Sicario",
-   id: "_Sicario",
-   synopsis: "The drug war on the U.S.-Mexico border has escalated as the cartels have begun trafficking terrorists across the US border. To fight the war, federal agent Matt Graver re-teams with the mercurial Alejandro."
-},
+// {
+//    src: Sicario,
+//    title: "Sicario",
+//    alt: "Sicario",
+//    id: "_Sicario",
+//    synopsis: "The drug war on the U.S.-Mexico border has escalated as the cartels have begun trafficking terrorists across the US border. To fight the war, federal agent Matt Graver re-teams with the mercurial Alejandro."
+// },
 
-{
-   src: Siberia,
-   title: "Siberia",
-   alt: "Siberia",
-   id: "_Siberia",
-   synopsis: "When an American diamond trader's Russian partner goes missing, he journeys to Siberia in search of him, but instead begins a love affair."
-}];
+// {
+//    src: Siberia,
+//    title: "Siberia",
+//    alt: "Siberia",
+//    id: "_Siberia",
+//    synopsis: "When an American diamond trader's Russian partner goes missing, he journeys to Siberia in search of him, but instead begins a love affair."
+// }];
 
-export default Movies;
+// export default Movies;
