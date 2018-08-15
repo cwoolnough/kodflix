a7048d91ac7bf0c357020d6f0bcf924245f3ee49
cwoolnough <cwoolnoughchamorro@googlemail.com>
2018-08-15 15:18:37 +0100

    Connect the frontend and backend servers #22

--- src/Frontend/Gallery/Gallery.js
+++ src/Frontend/Gallery/Gallery.js
@@ -4,11 +4,22 @@ import './../Cover/Cover.css';
 import Movies from './../GalleryGet/GalleryGet';
 
 class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: []
+        }
+    }
+     componentDidMount() {
+        fetch('/rest/movies')
+        .then(res => res.json())
+        .then(movies => this.setState({movies}));
+    }
+
    render() {
       return (
          <div className="container-row">
-            { Movies.map(movie => <Cover src={movie.src} title={movie.title} alt={movie.title} id={movie.id} key={movie.id} />
-            )}  
+            { Movies.map(movie => <Cover src={movie.src} title={movie.title} alt={movie.title} id={movie.id} key={movie.id} />)}  
          </div>
       );
    }
